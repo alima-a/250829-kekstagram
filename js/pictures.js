c600753d7cc52fcd2951c81fc9e55058aab47882
Алима <flemethett@gmail.com>
2018-09-13 15:42:16 +0700

    Задание 13.1

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var DESCRIPTIONS_SUM = 25;
+var PHOTOS_QUANITY = 25;
 var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var DESCRIPTIONS = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
-var PHOTOS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-var MIN_LIKES_NUM = 15;
-var MAX_LIKES_NUM = 200;
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
 
 // Найдем элементы
 var pictures = document.querySelector('.pictures');
@@ -23,64 +22,117 @@ var getRandomElement = function (array) {
   return array[getRandomNum(0, array.length - 1)];
 };
 
-// Функция перебора массива c фото
-var getRandomPhoto = function (array) {
-  for (var i = 0; i < array.length; i++) {
-    var photoNum = array[i];
+// Функция получения рандомного комментария
+var getRandomComment = function (from, to) {
+
+  var commentsArray = [];
+
+  for (var i = 0; i < to; i++) {
+    var comment = COMMENTS[getRandomNum(0, COMMENTS.length)];
+    var randomNumber = getRandomNum(0, COMMENTS.length);
+
+    if (randomNumber % 2) {
+      commentsArray.push(comment);
+    } else {
+      comment += COMMENTS[getRandomNum(0, COMMENTS.length)];
+      commentsArray.push(comment);
+    }
   }
-  return photoNum;
+  return commentsArray;
 };
 
 // Функция получения массива описаний фото
-var photosDescriptions = [];
+var photos = [];
 var generateDescriptions = function (amount) {
   for (var i = 0; i < amount; i++) {
-    photosDescriptions[i] = {
-      url: 'photos/' + getRandomPhoto(PHOTOS) + '.jpg',
-      likes: getRandomNum(MIN_LIKES_NUM, MAX_LIKES_NUM),
-      comments: getRandomElement(COMMENTS),
+    photos[i] = {
+      url: 'photos/' + (i + 1) + '.jpg',
+      likes: getRandomNum(MIN_LIKES, MAX_LIKES),
+      comments: getRandomComment(0, COMMENTS.length),
       description: getRandomElement(DESCRIPTIONS)
     };
   }
 };
 
 // Получаем массив описаний
-generateDescriptions(DESCRIPTIONS_SUM);
+generateDescriptions(PHOTOS_QUANITY);
 
 // DOM-элемент заполняем данными из массива
-var renderDescription = function (description) {
-  var descriptionsElement = pictureTemplate.cloneNode(true);
+var renderPhoto = function (photo) {
+  var photoElement = pictureTemplate.cloneNode(true);
 
-  descriptionsElement.querySelector('.picture__img').src = description.url;
-  descriptionsElement.querySelector('.picture__likes').textContent = description.likes;
-  descriptionsElement.querySelector('.picture__stat--comments').textContent = description.comments.length;
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__likes').textContent = photo.likes;
+  photoElement.querySelector('.picture__stat--comments').textContent = photo.comments.length;
 
-  return descriptionsElement;
+  return photoElement;
 };
 
-renderDescription(photosDescriptions);
+renderPhoto(photos);
 
 // Отрисуем сгенерированные DOM-элементы в блок
-var renderDescriptions = function () {
+var appendPhotos = function () {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < photosDescriptions.length; i++) {
-    fragment.appendChild(renderDescriptions(photosDescriptions[i]));
+  for (var i = 0; i < photos.length; i++) {
+    fragment.appendChild(renderPhoto(photos[i]));
   }
   pictures.appendChild(fragment);
 };
 
-renderDescriptions(photosDescriptions);
+appendPhotos(photos);
 
 // Делаем видимым блок
 var bigPicture = document.querySelector('.big-picture');
 bigPicture.remove('hidden');
 
+// Функция для отображения комментария
+var getComments = function (сomments) {
+  var fragmentOfComments = document.createDocumentFragment();
+
+  var socialComments = document.querySelector('.social__comments');
+  socialComments.innerHTML = '';
+
+  for (var i = 0; i < сomments.length; i++) {
+    var li = document.createElement('li');
+    li.classList.add('social__comment');
+
+    var img = document.createElement('img');
+    img.classList.add('social__picture');
+    img.setAttribute('src', 'img/avatar-' + getRandomNum(1, 6) + '.svg');
+    img.setAttribute('alt', 'Аватар комментатора фотографии');
+    img.setAttribute('width', '35');
+    img.setAttribute('height', '35');
+
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('social__text');
+    paragraph.textContent = сomments[i];
+
+    li.appendChild(img);
+    li.appendChild(paragraph);
+
+    fragmentOfComments.appendChild(li);
+
+    socialComments.appendChild(fragmentOfComments);
+  }
+  return socialComments;
+};
+
 // DOM-элемент заполняем данными из массива
-var renderBigPicture = function (photo) {
-  bigPicture.querySelector('.big-picture__img img').src = description.url;
-  bigPicture.querySelector('.comments-count').textContent = description.comments.length;
-  bigPicture.querySelector('.likes-count').textContent = description.likes;
-  bigPicture.querySelector('.social__caption').textContent = description.description;
+var renderBigPicture = function (array) {
+  bigPicture.querySelector('.big-picture__img img').src = array.url;
+  bigPicture.querySelector('.comments-count').textContent = array.comments.length;
+  bigPicture.querySelector('.likes-count').textContent = array.likes;
+  bigPicture.querySelector('.social__caption').textContent = array.description;
+  getComments(array.comments);
 
   return bigPicture;
 };
+
+// Отрисовываем
+renderBigPicture(photos[0]);
+
+// Прячем блоки
+var socialСommentСount = document.querySelector('.social__comment-count');
+socialСommentСount.classList.add('visually-hidden');
+var commentsloader = document.querySelector('.comments-loader');
+commentsloader.classList.add('visually-hidden');
